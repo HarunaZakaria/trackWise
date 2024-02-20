579c5ecb328c325579bce5d0bbf63261712c9b1d
harunazakaria <Harunzy55@gmail.com.com>
2024-02-20 10:01:27 +0000

    change all files to php

---
+++ home.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="main-css/home.css" />
+    <title>trackWise</title>
+  </head>
+  <body>
+    
+    <div class="container">
+    <h1>Welcome to trackWise</h1>
+    <div class="list-img">
+      <div class="list">
+        <ul>
+          <li><a href="#">All Products</a></li>
+          <li><a href="addproducts.php">Add Products</a></li>
+          <li><a href="#">Update Products</a></li>
+          <li><a href="#">Delete Products</a></li>
+          <li><a href="#">Point of Sale</a></li>
+        </ul>
+      </div>
+      <div id="img">
+        <img src="images/h.jpg" alt="image of a mall" />
+      </div>
+    </div>
+  </div>
+  
+  </body>
+</html>
+
+
